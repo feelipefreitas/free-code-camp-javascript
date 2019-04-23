df21575d39c5de816a212c9a6391f02602f72ab2
feelipefreitas <feelipe.freitas97@gmail.com>
2019-04-23 11:18:13 -0300

    Understand String Immutability

---
+++ Basic JavaScript/Understand String Immutability.js
@@ -0,0 +1,8 @@
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
+
+//Não é possível mudar somente uma unica letra de uma string 'myStr[0] = F', somente o valor completo dela
