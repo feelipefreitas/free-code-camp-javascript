62149610a1d1e2ef420a5f9d5828bf27745634ae
feelipefreitas <feelipe.freitas97@gmail.com>
2019-04-24 22:35:30 -0300

    Adding

--- Basic Algorithm Scripting/Find the Longest Word in a String.js
+++ Basic Algorithm Scripting/Find the Longest Word in a String.js
@@ -1,5 +1,12 @@
+//Return the string
 'Felipe Freitas'.split(' ').reduce((acc, curr) => {
     acc = curr;
     if (curr.length > acc.length) acc = curr;
     return acc;
 }, '');
+
+//Return the number
+'Felipe Freitas'.split(' ').reduce((acc, curr) => {
+    if (curr.length > acc) acc = curr.length;
+    return acc;
+}, 0);
